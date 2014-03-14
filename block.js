6775fd895c21a62dbc8819cc6a4deaec5b75d57f
Yuki KAN <re@pixely.jp>
2014-03-14 20:31:56 +0900

    first commit.

---
+++ block.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function () {
+	document.body.innerHTML = '<small style="color:#777;">Blocked by Adult Ad Blocker.</small>';
+	document.body.style.background = '#eee';
+	document.body.style.padding = '10px';
+});
